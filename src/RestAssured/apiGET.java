1d8272d1b36cf4764135052ee888ce2ce7efbcb2
johanhartono <johanhartono@gmail.com>
2024-11-06 16:24:12 +0700

    first commit

---
+++ src/RestAssured/apiGET.java
@@ -0,0 +1,35 @@
+
+package RestAssured;
+
+import static io.restassured.RestAssured.given;
+import org.junit.jupiter.api.Test;
+
+public class apiGET
+{
+	String url1 = "https://reqres.in/api/users";
+	String url2 = "https://airportgap.com/api/airports";
+
+	@Test
+	public void getListOfData() {
+		String EndPoint = url1;
+		var response = given().when().get(EndPoint).then().assertThat().statusCode(200);
+		response.log().all();
+	}
+	@Test
+	
+	public void getaData() {
+		String EndPoint = url1 +"/2";
+		var response = given().when().get(EndPoint).then().assertThat().statusCode(200);
+		response.log().all();
+	}
+
+	
+	@Test
+	public void getQueryaData() {
+		String EndPoint = url1;
+		var response = given().queryParam("id",3).when().get(EndPoint).then().assertThat().statusCode(200);
+		//var response = given().when().get(EndPoint).then();
+		
+		response.log().all();
+	}
+}
